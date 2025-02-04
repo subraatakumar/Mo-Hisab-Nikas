a4611ff4f93dd952d22e686f2335c9a45393c1f4
subraatakumar <business.ttrc@gmail.com>
2025-02-04 19:45:07 +0530

    initial commit

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,12 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+const config = {
+  watchFolders: [path.resolve(__dirname, 'node_modules')],
+  resolver: {
+    blacklistRE: /node_modules\/.*\/node_modules\/.*/, // Avoid duplicate dependencies
+  },
+  maxWorkers: 4, // Adjust based on CPU cores
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
